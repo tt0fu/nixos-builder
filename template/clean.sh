90abd0ea5ce8110f8862449db03c0910ac0c1f6b
ttofu <tt0fu@users.noreply.github.com>
2026-08-29 01:00:40 +0300

    Remove old cleaning code

--- template/clean.sh
+++ template/clean.sh
@@ -1,17 +1,5 @@
 before=$(df -k . | awk 'NR==2 {print $4}')
 
-# sudo nix-collect-garbage -d
-# nix-collect-garbage -d
-
-# sudo nix store gc
-# nix store gc
-
-# sudo nix profile wipe-history
-# nix profile wipe-history
-
-# sudo nix store optimise
-# nix store optimise
-
 sudo nh clean all
 nh clean all
 
